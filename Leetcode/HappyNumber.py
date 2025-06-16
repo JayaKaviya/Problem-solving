0cce852bcaf3ff8d1783a5f0baff77c6fe9ba972
JayaKaviya <kavyamanz@gmail.com>
2025-06-16 23:40:24 +0530

    happy number

--- Leetcode/HappyNumber.py
+++ Leetcode/HappyNumber.py
@@ -6,7 +6,21 @@ if m in l:
 else:  
     while n!=1: 
         n=sum([int(i)**2 for i in str(n)]) 
-    print("true")
+    print("true") 
+     
+###############################
+
+class Solution:
+    def isHappy(self, n: int) -> bool: 
+        seen=set() 
+        while n!=1:
+            if n in seen:
+                return False 
+            seen.add(n) 
+            n=sum(int(x)**2 for x in str(n)) 
+        return True
+
+      
         
    
 
